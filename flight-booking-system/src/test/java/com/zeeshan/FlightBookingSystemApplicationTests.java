b29f1d3984ff204385f5e43d41879f98277efec7
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-02-17 17:42:37 +0530

    FlightBooking test class

---
+++ flight-booking-system/src/test/java/com/zeeshan/FlightBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeeshan;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
