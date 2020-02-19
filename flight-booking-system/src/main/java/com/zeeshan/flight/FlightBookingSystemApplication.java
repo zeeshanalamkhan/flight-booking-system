6b3e052634e520965871f00c5be8ff66b2bb75b3
ZEESHAN KHAN <zeeshan.maths@gmail.com>
2020-02-19 15:55:30 +0530

    support added to deploy a .war file to external server

--- flight-booking-system/src/main/java/com/zeeshan/flight/FlightBookingSystemApplication.java
+++ flight-booking-system/src/main/java/com/zeeshan/flight/FlightBookingSystemApplication.java
@@ -2,12 +2,19 @@ package com.zeeshan.flight;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FlightBookingSystemApplication {
+public class FlightBookingSystemApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlightBookingSystemApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+
+		return builder.sources(FlightBookingSystemApplication.class);
+	}
 }
